f6664fe10460607f2b35d5b70ad43ab1f4900c30
solaretech <solaretech.kk@gmail.com>
2020-08-26 19:12:41 +0900

    Update test.js by HOC

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-const arr = [1, 3, 2, 4, 6, 5, 8, 7];
+const hof = (ex, fn) => {
+  return n => fn(n + ex);
+};
 
-console.log(arr.map(n => n * 2));
-console.log(arr.filter(n => n%3 === 0));
-console.log(arr.find(n => n > 4));
-console.log(arr.every(n => n !== 0));
-console.log(arr.some(n => n > 8));
-console.log(arr.includes(5));
-console.log(arr.reduce((n, m) => n + m));
-console.log(arr.sort((n, m) => n > m ? 1 : -1));
+const plusOneDouble = hof(1, n => n * 2);
+console.log(plusOneDouble(4));
