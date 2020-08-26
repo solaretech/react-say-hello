b5e9aa0288c666312bdc975222f60e3aae577378
solaretech <solaretech.kk@gmail.com>
2020-08-26 18:10:46 +0900

    Update Test.js

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-const arr1 = ["A", 'B', 'C'];
-const arr2 = [...arr1, 'D', 'E'];
-console.log(arr2);
-
-const obj1 = { a: 1, b: 2, c: 3 };
-const obj2 = { ...obj1, d: 4, e: 5};
-console.log(obj2);
-console.log(obj2.a);
+const foo = 65536;
+const obj = { foo, bar: 4096 };
+console.log(obj);
