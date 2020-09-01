cb44e543207d82720cc5199645c48ed16360cc1a
solaretech <solaretech.kk@gmail.com>
2020-09-01 17:46:17 +0900

    Update test.js by triple function

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
-const multi = (n, m) => n * m;
-console.log(multi(2, 4));
+const multi = n => m => n * m;
+console.log(multi(3)(5));
 
-const curriedMulti = n => {
-  return m => n * m;
-}
-console.log(curriedMulti(2)(4));
-
-const simpleCurriedMulti = n => m => n * m;
-console.log(simpleCurriedMulti(2)(4));
+const triple = multi(3);
+console.log(triple(5));
