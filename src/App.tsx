5665e06e069c6803628cc04a34046efd01dbd0b6
solaretech <solaretech.kk@gmail.com>
2020-09-29 16:27:21 +0900

    Environment Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render (){
+    const logoAttrs = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo
+    };
+    const title = "こんにちは";
+    const targets = ["World", "Kanae", "Yukina"];
+
+    return(
+      <div className="App">
+        <header className="App-header">
+          {
+          // コ メ ン ト は こ う 書 く
+          }
+          <img {...logoAttrs} />
+          {title && <p>{title}</p>}
+          {targets.map(target => (
+            <p>Hello, {target}!</p>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
